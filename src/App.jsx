dff8ffdc64619194b61e29dc69bf2157586a5c40
Swayam Prakash <swayamprakashgali930@gmail.com>
2024-12-19 12:43:03 +0530

    My Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./Components/Navbar";
 import Section from "./Components/Section";
 import Section1 from "./Components/Section1";
 import Services from "./Components/Services";
+import Test from "./Components/Test";
 
 function App(){
     return(
@@ -17,7 +18,7 @@ function App(){
         <Services/>
         <Contact/>
         <Faculty/>
-
+        <Test/>
      </div>
     )
 }
---
+++ src/Components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      <h1>This  is For Testing Purpose</h1>
+    </div>
+  )
+}
+
+export default Test
